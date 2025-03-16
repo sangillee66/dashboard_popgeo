96f523930fec4a87c10e7418e93715fcc967caa5
Sang-Il Lee <si_lee@snu.ac.kr>
2025-03-16 22:28:22 +0900

    aa

--- backworks.R
+++ backworks.R
@@ -3,6 +3,9 @@
 # Libraries ---------------------------------------------------------------
 library(tidyverse)
 library(readxl)
+library(rnaturalearth)
+library(sf)
+library(tmap)
 
 # World Population Prospects 2024 -----------------------------------------
 new_names <- c("index", "variant", "region_name", "notes", "location_code", 
@@ -52,4 +55,64 @@ wpp_2024_new <- wpp_2024 |>
   )
 
 write_rds(wpp_2024_new, "wpp_2024.rds")
+
+
+# 전세계 국가 참조 지도 ------------------------------------------------------------
+
+countries.m <- st_read("D:/My R/World Data Manupulation/NaturalEarth/new_2_ne_50m_admin_0_countries.shp") 
+# tiny.m <- ne_download(scale = "medium", type = "tiny_countries", category = "cultural", returnclass = "sf")
+
+world.region.code <- read_excel("D:/My R/Population Geography/0 Population Data/World_Region_Code.xlsx", sheet = 1, col_names = TRUE)
+
+countries.m.region <- countries.m |> 
+  left_join(
+    world.region.code, join_by(ISO_N3_CD == `M49 Code`)
+  ) |> 
+  mutate(
+    New_New_Sub_SDG_NM = case_match(
+      New_New_Sub_SDG_NM,
+      "CENTRAL AND SOUTHERN ASIA" ~ "Central and Southern Asia",
+      "EUROPE, NORTHERN AMERICA, AUSTRALIA, AND NEW ZEALAND" ~ "Europe, Northern America, Australia, and New Zealand",
+      "ANTARCTICA" ~ "Antarctica",
+      "NORTHERN AFRICA AND WESTERN ASIA" ~ "Northern Afraica and Western Asia",
+      "OCEANIA (EXC AUSTRALIA AND NEW ZEALAND)" ~ "Oceania (Excluding Australia and New Zealand)",
+      "SUB-SAHARAN AFRICA" ~ "Sub-Saharan Africa",
+      "LATIN AMERICA AND THE CARIBBEAN" ~ "Latin America and THE Caribbean",
+      "EASTERN AND SOUTH-EASTERN ASIA" ~ "Eastern and South-Eastern Asia" 
+    )
+  )
+
+tmap_mode("view")
+tm_shape(countries.m.region) + tm_polygons( 
+  alpha = 0, border.alpha = 0, id = "NAME_KO", 
+  popup.vars = c(
+    "ISO3" = "ISO_A3", 
+    "NAME" = "NAME_LONG",
+    "UN Subregion" = "New_UN_All_Subregion_NM",
+    "UN Region" = "New_UN_Region_NM",
+    "SDG Region" = "New_New_Sub_SDG_NM"
+    )
+  ) 
+  
+
+# 우리나라 시군구 단위 -------------------------------------------------------------
+
+SIGUNGU1.shp <- st_read("D:/My R/Korean Administrative Areas/행정구역 셰이프 파일/3 Generalization/2023_2Q/NOT_MOVE/SIGUNGU1_NM_2023_2Q_GEN_0030.shp", options = "ENCODING=CP949")
+SIDO_Polyline.shp <- st_read("D:/My R/Korean Administrative Areas/행정구역 셰이프 파일/3 Generalization/2023_2Q/NOT_MOVE/SIDO_Polyline_NM_2023_2Q_GEN_0030.shp", options = "ENCODING=CP949")
+
+SIGUNGU1.shp <- SIGUNGU1.shp |> 
+  mutate(
+    SGG1_CD = as.character(SGG1_CD)
+  )
+
+tmap_mode("view")
+tm_shape(SIGUNGU1.shp) + tm_fill(
+  col = "gray90", alpha = 0.2, id = "SGG1_FNM", 
+  popup.vars = c(
+    "영어 이름" = "Eng_NM",
+    "코드번호" = "SGG1_CD" 
+  ) 
+  ) + tm_borders() +
+  tm_shape(SIDO_Polyline.shp) + tm_lines(col = "black", lwd = 2)
+
 
